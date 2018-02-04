7fb0392be06912ec78d1d97be99a2bede0769253
brycecorbitt <bscorbitt@wpi.edu>
2018-02-04 13:42:13 -0500

    Changes to be committed: 	new file:   src/main/java/org/dselent/scheduling/server/controller/CourseController.java 	new file:   src/main/java/org/dselent/scheduling/server/controller/RequestController.java 	new file:   src/main/java/org/dselent/scheduling/server/controller/ScheduleController.java 	new file:   src/main/java/org/dselent/scheduling/server/controller/SectionController.java

---
+++ src/main/java/org/dselent/scheduling/server/controller/CourseController.java
@@ -0,0 +1,16 @@
+package org.dselent.scheduling.server.controller;
+
+import java.util.Map;
+
+//Import request classes
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/course")
+public interface CourseController
+{    
+    @RequestMapping(method=RequestMethod.POST, value=)
+	public ResponseEntity<String> register(@RequestBody Map<String, String> request) throws Exception;
+}
