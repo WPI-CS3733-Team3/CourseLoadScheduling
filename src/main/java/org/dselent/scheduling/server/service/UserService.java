ae3e71ce0f4fec0ecf7a5bb2c783c3848b078d8b
Matt <mjschueler@gmail.com>
2018-02-08 22:05:35 -0500

    fixed duplicate line

--- src/main/java/org/dselent/scheduling/server/service/UserService.java
+++ src/main/java/org/dselent/scheduling/server/service/UserService.java
@@ -25,7 +25,6 @@ public interface UserService
 	 * @throws SQLException
 	 */
 	public List<Integer> createUser(String email, String password) throws SQLException;
-	public List<Integer> editUser(int id, String fname, String lname, String pass) throws SQLException;
     public User login(String email, String password) throws SQLException;
 	public List<Integer> editUser(int id, String fname, String lname, String pass) throws SQLException;
     public List<Integer> promote(String email) throws SQLException;
