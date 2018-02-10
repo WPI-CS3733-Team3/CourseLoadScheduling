8ab59796b01b4073b74af9015c7a79913d4434bb
Frank Campanelli <rainbowturban412@gmail.com>
2018-02-09 22:45:36 -0500

    added success.add(...) to JSONResponseCreator
    24k JSON magic

--- src/main/java/org/dselent/scheduling/server/miscellaneous/JsonResponseCreator.java
+++ src/main/java/org/dselent/scheduling/server/miscellaneous/JsonResponseCreator.java
@@ -14,36 +14,36 @@ public class JsonResponseCreator
 		SUCCESS,
 		ERROR;
 	}
-	
+
 	public static String getJSONResponse(ResponseKey responseKey, Object responseObjectList) throws JsonProcessingException
 	{
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put(responseKey.toString().toLowerCase(), responseObjectList);
-		    
+
 		return mapper.writeValueAsString(map);
 	}
-	
+
 	public static String getJSONResponse(ResponseKey responseKey, List<Object> responseObjectList) throws JsonProcessingException
 	{
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put(responseKey.toString().toLowerCase(), responseObjectList);
-		    
+
 		return mapper.writeValueAsString(map);
 	}
-	
+
 	public static String getJSONResponse(List<Object> success, List<Object> error) throws JsonProcessingException
 	{
 		return getJSONResponse(success, error, new ObjectMapper());
 	}
-		  
+
 	public static String getJSONResponse(List<Object> success, List<Object> error,  ObjectMapper mapper) throws JsonProcessingException
 	{
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put(ResponseKey.SUCCESS.toString().toLowerCase(), success);
 		map.put(ResponseKey.ERROR.toString().toLowerCase(), error);
-		    
+
 		return mapper.writeValueAsString(map);
 	}
 }
