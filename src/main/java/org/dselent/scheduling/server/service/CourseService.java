7ff16c2b617f583cdf539918a72ba2e16328974a
kms80 <xuebai80@yahoo.com>
2018-02-09 22:48:10 -0500

    Here you go

--- src/main/java/org/dselent/scheduling/server/service/CourseService.java
+++ src/main/java/org/dselent/scheduling/server/service/CourseService.java
@@ -1,5 +1,15 @@
 package org.dselent.scheduling.server.service;
 
-public interface CourseService {
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+
+public class CourseService {
+    public List<Integer> create(String courseName, String courseNumber, int frequency) throws SQLException;
+    public List<Integer> edit(EditCourseDto editCourseDto) throws SQLException;
+   public Course removeCourse(Integer id);
+   public viewAllCourse(Integer id);
+       public viewOneCourse(Integer id);
 }
