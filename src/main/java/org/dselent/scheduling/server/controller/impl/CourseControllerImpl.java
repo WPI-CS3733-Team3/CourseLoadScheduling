8ea6edaf316315400ac068ec8a96604d1d6cc616
dselent <selentd@gmail.com>
2018-02-11 19:29:08 -0500

    Added @Controller annotation to CourseControllerImpl.java

--- src/main/java/org/dselent/scheduling/server/controller/impl/CourseControllerImpl.java
+++ src/main/java/org/dselent/scheduling/server/controller/impl/CourseControllerImpl.java
@@ -14,10 +14,11 @@ import org.dselent.scheduling.server.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@Controller
 public class CourseControllerImpl implements CourseController{
 	
 	@Autowired
