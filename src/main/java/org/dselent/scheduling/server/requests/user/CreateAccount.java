6d9b58e145cd74907250ca201f26a892fc7e0d4f
Frank Campanelli <rainbowturban412@gmail.com>
2018-02-25 18:22:46 -0500

    this bitch empty, YEET

--- src/main/java/org/dselent/scheduling/server/requests/user/CreateAccount.java
+++ src/main/java/org/dselent/scheduling/server/requests/user/CreateAccount.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CreateAccount
 {
 	public static final RequestMethod REQUEST_TYPE = RequestMethod.POST;
-	public static final String REQUEST_NAME = "user/create";
+	public static final String REQUEST_NAME = "create";
 	private static final List<HeaderKey> HEADER_KEY_LIST;
 	private static final List<ParameterKey> PARAMETER_KEY_LIST;
 	private static final List<BodyKey> BODY_KEY_LIST;
